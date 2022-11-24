d17803c0d07735b526f37a971c69b2e4795ffbf6
pedrogiampietro <pedrogiampietro@hotmail.com>
2022-11-24 08:30:55 -0300

    add: hook local storage to setTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { useState, createContext } from "react";
-import { ThemeProvider } from "styled-components";
-import { GlobalStyle } from "./assets/styles/globalStyles";
-import { darkTheme, lightTheme } from "./assets/styles/theme";
+import { useState, useEffect, createContext } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './assets/styles/globalStyles'
+import { darkTheme, lightTheme } from './assets/styles/theme'
 
-import { AuthRoutes } from "./authRoutes";
+import { AuthRoutes } from './authRoutes'
+import { useLocalStorage } from './hooks/useLocalStorage'
 
 interface IContextData {
-  theme: string;
-  setTheme: React.Dispatch<React.SetStateAction<string>>;
+	theme: string
+	setTheme: React.Dispatch<React.SetStateAction<string>>
 }
 
-export const ThemeContext = createContext({} as IContextData);
+export const ThemeContext = createContext({} as IContextData)
 
 export function App() {
-  const [theme, setTheme] = useState("light");
-  const themeStyle = theme === "light" ? lightTheme : darkTheme;
+	const [theme, setTheme] = useLocalStorage('imDesk@CurrentTheme', 'light')
+	const themeStyle = theme === 'light' ? lightTheme : darkTheme
 
-  return (
-    <ThemeContext.Provider value={{ setTheme, theme } as any}>
-      <ThemeProvider theme={themeStyle}>
-        <GlobalStyle />
+	return (
+		<ThemeContext.Provider value={{ setTheme, theme } as any}>
+			<ThemeProvider theme={themeStyle}>
+				<GlobalStyle />
 
-        <AuthRoutes />
-      </ThemeProvider>
-    </ThemeContext.Provider>
-  );
+				<AuthRoutes />
+			</ThemeProvider>
+		</ThemeContext.Provider>
+	)
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,135 +1,136 @@
-import { useContext, useRef, useState } from "react";
-import * as S from "./styles";
+import { useContext, useRef, useState } from 'react'
+import * as S from './styles'
 
 // import { logoSVG } from "../../assets";
 
 import {
-  AiOutlineApartment,
-  AiOutlineHome,
-  AiOutlineLeft,
-  AiOutlineSearch,
-  AiOutlineSetting,
-} from "react-icons/ai";
-import { MdLogout, MdOutlineAnalytics } from "react-icons/md";
-import { BsPeople } from "react-icons/bs";
+	AiOutlineApartment,
+	AiOutlineHome,
+	AiOutlineLeft,
+	AiOutlineSearch,
+	AiOutlineSetting,
+} from 'react-icons/ai'
+import { MdLogout, MdOutlineAnalytics } from 'react-icons/md'
+import { BsPeople } from 'react-icons/bs'
 
-import { ThemeContext } from "./../../App";
-import { useLocation } from "react-router-dom";
+import { ThemeContext } from './../../App'
+import { useLocation } from 'react-router-dom'
 
 export function Sidebar() {
-  const searchRef = useRef(null);
-  const { setTheme, theme } = useContext(ThemeContext);
+	const searchRef = useRef(null)
+	const { setTheme, theme } = useContext(ThemeContext)
 
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const { pathname } = useLocation();
+	const [sidebarOpen, setSidebarOpen] = useState(false)
+	const { pathname } = useLocation()
 
-  const searchClickHandler = () => {
-    if (!sidebarOpen) {
-      setSidebarOpen(true);
-      // searchRef.current.focus();
-    } else {
-      // search functionality
-    }
-  };
+	const searchClickHandler = () => {
+		if (!sidebarOpen) {
+			setSidebarOpen(true)
+			// searchRef.current.focus();
+		} else {
+			// search functionality
+		}
+	}
 
-  const secondaryLinksArray = [
-    {
-      label: "Settings",
-      icon: <AiOutlineSetting />,
-    },
-    {
-      label: "Logout",
-      icon: <MdLogout />,
-    },
-  ];
+	const toggleTheme = () => {
+		setTheme((p) => (p === 'light' ? 'dark' : 'light'))
+	}
 
-  return (
-    <S.Sidebar isOpen={sidebarOpen}>
-      <>
-        <S.SidebarButton
-          isOpen={sidebarOpen}
-          onClick={() => setSidebarOpen((p) => !p)}
-        >
-          <AiOutlineLeft />
-        </S.SidebarButton>
-      </>
-      <S.Logo>{/* <img src={logoSVG} alt="logo" /> */}</S.Logo>
-      <S.Search
-        onClick={searchClickHandler}
-        style={!sidebarOpen ? { width: `fit-content` } : {}}
-      >
-        <S.SearchIcon>
-          <AiOutlineSearch />
-        </S.SearchIcon>
-        <input
-          ref={searchRef}
-          placeholder="Search"
-          style={!sidebarOpen ? { width: 0, padding: 0 } : {}}
-        />
-      </S.Search>
-      <S.Divider />
-      {linksArray.map(({ icon, label, notification, to }) => (
-        <S.LinkContainer key={label} isActive={pathname === to}>
-          <S.LinkStyle
-            to={to}
-            style={!sidebarOpen ? { width: `fit-content` } : {}}
-          >
-            <S.LinkIcon>{icon}</S.LinkIcon>
-            {sidebarOpen && (
-              <>
-                <S.LinkLabel>{label}</S.LinkLabel>
-                {/* if notifications are at 0 or null, do not display */}
-                {!!notification && (
-                  <S.LinkNotification>{notification}</S.LinkNotification>
-                )}
-              </>
-            )}
-          </S.LinkStyle>
-        </S.LinkContainer>
-      ))}
-      <S.Divider />
-      {secondaryLinksArray.map(({ icon, label }) => (
-        <S.LinkContainer key={label}>
-          <S.LinkStyle
-            to="/"
-            style={!sidebarOpen ? { width: `fit-content` } : {}}
-          >
-            <S.LinkIcon>{icon}</S.LinkIcon>
-            {sidebarOpen && <S.LinkLabel>{label}</S.LinkLabel>}
-          </S.LinkStyle>
-        </S.LinkContainer>
-      ))}
-      <S.Divider />
-      <S.Theme>
-        {sidebarOpen && <S.ThemeLabel>Dark Mode</S.ThemeLabel>}
-        <S.ThemeToggler
-          isActive={theme === "dark"}
-          onClick={() => setTheme((p) => (p === "light" ? "dark" : "light"))}
-        >
-          <S.ToggleThumb style={theme === "dark" ? { right: "1px" } : {}} />
-        </S.ThemeToggler>
-      </S.Theme>
-    </S.Sidebar>
-  );
+	const secondaryLinksArray = [
+		{
+			label: 'Settings',
+			icon: <AiOutlineSetting />,
+		},
+		{
+			label: 'Logout',
+			icon: <MdLogout />,
+		},
+	]
+
+	return (
+		<S.Sidebar isOpen={sidebarOpen}>
+			<>
+				<S.SidebarButton
+					isOpen={sidebarOpen}
+					onClick={() => setSidebarOpen((p) => !p)}
+				>
+					<AiOutlineLeft />
+				</S.SidebarButton>
+			</>
+			<S.Logo>{/* <img src={logoSVG} alt="logo" /> */}</S.Logo>
+			<S.Search
+				onClick={searchClickHandler}
+				style={!sidebarOpen ? { width: `fit-content` } : {}}
+			>
+				<S.SearchIcon>
+					<AiOutlineSearch />
+				</S.SearchIcon>
+				<input
+					ref={searchRef}
+					placeholder='Search'
+					style={!sidebarOpen ? { width: 0, padding: 0 } : {}}
+				/>
+			</S.Search>
+			<S.Divider />
+			{linksArray.map(({ icon, label, notification, to }) => (
+				<S.LinkContainer key={label} isActive={pathname === to}>
+					<S.LinkStyle
+						to={to}
+						style={!sidebarOpen ? { width: `fit-content` } : {}}
+					>
+						<S.LinkIcon>{icon}</S.LinkIcon>
+						{sidebarOpen && (
+							<>
+								<S.LinkLabel>{label}</S.LinkLabel>
+								{/* if notifications are at 0 or null, do not display */}
+								{!!notification && (
+									<S.LinkNotification>{notification}</S.LinkNotification>
+								)}
+							</>
+						)}
+					</S.LinkStyle>
+				</S.LinkContainer>
+			))}
+			<S.Divider />
+			{secondaryLinksArray.map(({ icon, label }) => (
+				<S.LinkContainer key={label}>
+					<S.LinkStyle
+						to='/'
+						style={!sidebarOpen ? { width: `fit-content` } : {}}
+					>
+						<S.LinkIcon>{icon}</S.LinkIcon>
+						{sidebarOpen && <S.LinkLabel>{label}</S.LinkLabel>}
+					</S.LinkStyle>
+				</S.LinkContainer>
+			))}
+			<S.Divider />
+			<S.Theme>
+				{sidebarOpen && <S.ThemeLabel>Dark Mode</S.ThemeLabel>}
+				<S.ThemeToggler isActive={theme === 'dark'} onClick={toggleTheme}>
+					<S.ToggleThumb style={theme === 'dark' ? { right: '1px' } : {}} />
+				</S.ThemeToggler>
+			</S.Theme>
+		</S.Sidebar>
+	)
 }
 
 const linksArray = [
-  {
-    label: "Inicio",
-    icon: <AiOutlineHome />,
-    to: "/dashboard",
-    notification: 0,
-  },
-  {
-    label: "Estatisticas",
-    icon: <MdOutlineAnalytics />,
-    to: "/statistics",
-    notification: 3,
-  },
-  {
-    label: "Tickets",
-    icon: <BsPeople />,
-    to: "/tickets",
-    notification: 0,
-  },
-];
+	{
+		label: 'Inicio',
+		icon: <AiOutlineHome />,
+		to: '/dashboard',
+		notification: 0,
+	},
+	{
+		label: 'Estatisticas',
+		icon: <MdOutlineAnalytics />,
+		to: '/statistics',
+		notification: 3,
+	},
+	{
+		label: 'Tickets',
+		icon: <BsPeople />,
+		to: '/tickets',
+		notification: 0,
+	},
+]
